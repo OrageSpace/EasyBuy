98a5b1cc64b1f28bfad0874aebe946a800bf82fd
QingYun-Eclipse <1787286700@qq.com>
2019-07-27 09:28:56 +0800

    第三次提交
    1.修改了易买网--首页
    2.添加了页面image、css、js资源
    3.创建了HomeServlet用于处理首页商品分类信息
    
    Change-Id: I11afa5075840574c71e4fa61d05e263586f4be12
    Signed-off-by: QingYun-Eclipse <1787286700@qq.com>

---
+++ src/cn/easybuy/web/HomeServlet.java
@@ -0,0 +1,51 @@
+package cn.easybuy.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import cn.easybuy.entity.ProductCategory;
+import cn.easybuy.service.product.ProductCategoryService;
+import cn.easybuy.service.product.ProductCategoryServiceImpl;
+
+@WebServlet(urlPatterns= {"/Home"},name="Home")
+public class HomeServlet extends HttpServlet {
+	
+	private ProductCategoryService pcs=null;
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		this.doPost(request, response);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		//设置请求的编码 防止请求中文乱码
+		request.setCharacterEncoding("UTF-8");
+		//设置响应内容类型 以及编码格式
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=UTF-8");
+		
+		//获取请求中的参数值
+		String parentId="1";
+		
+		pcs=new ProductCategoryServiceImpl();
+		
+		//调用查询商品分类信息的方法
+		List<ProductCategory> pcList=pcs.queryAllProductCategory(parentId);
+		
+		//将查询到的内容保存到request作用域中
+		request.setAttribute("pcList", pcList);
+		
+		//将页面转发到/pre/index.jsp
+		request.getRequestDispatcher("pre/index.jsp").forward(request, response);
+	}
+}
